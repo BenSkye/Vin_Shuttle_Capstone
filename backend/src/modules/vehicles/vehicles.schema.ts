0b3ab8fb7252b2dba67bbc8a958ffeede012da40
laptop-m8du3f9f <quangtran214203@gmail.com>
2025-01-15 16:32:20 +0700

    Update backend dependencies and configuration
    - Upgraded @nestjs/common, @nestjs/core, @nestjs/platform-express to version 10.4.15.
    - Updated reflect-metadata to version 0.2.2.
    - Modified ESLint configuration to disable prettier rule.
    - Enhanced app.module.ts to include ConfigModule and MongooseModule for MongoDB connection.
    - Added VehicleCategoryModule and VehiclesModule to the imports in app.module.ts.
    - Updated main.ts to enable CORS for localhost and changed the default port to 2025.

---
+++ backend/src/modules/vehicles/vehicles.schema.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument, Types } from "mongoose";
+
+export type VehicleDocument = HydratedDocument<Vehicle>;
+
+@Schema({ collection: 'Vehicles', timestamps: true })
+export class Vehicle {
+    @Prop({ required: true })
+    name: string;
+
+    @Prop({ type: Types.ObjectId, ref: 'VehicleCategory', required: true })
+    categoryId: Types.ObjectId;
+
+    @Prop({ required: true })
+    licensePlate: string;  // Biển số xe
+
+    @Prop({ default: true })
+    isActive: boolean;     // Trạng thái hoạt động
+
+    @Prop({ default: 'available' })
+    status: string;       // Trạng thái: available, in-use, maintenance
+}
+
+export const VehicleSchema = SchemaFactory.createForClass(Vehicle); 
