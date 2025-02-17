da20e4ffecd84db21a02df6ca95dd03dd2d5408b
luan0811 <nguyentranminhluan9a10@gmail.com>
2025-02-17 17:19:58 +0700

    setup reactnative

--- frontend-driver/App.tsx
+++ frontend-driver/App.tsx
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { View, Text } from 'react-native';
-import "./global.css"
+
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-blue-600">
-        Driver App with NativeWind
+        Driver App with NativeWind 12345
       </Text>
       <StatusBar style="auto" />
     </View>
--- frontend-driver/metro.config.js
+++ frontend-driver/metro.config.js
@@ -1,6 +1,16 @@
 const { getDefaultConfig } = require("expo/metro-config")
 const { withNativeWind } = require("nativewind/metro")
 
-const config = getDefaultConfig(__dirname)
+const config = getDefaultConfig(__dirname, {
+  isCSSEnabled: true
+})
 
-module.exports = withNativeWind(config, { input: "./global.css" })
+module.exports = withNativeWind(config, { 
+  input: "./src/styles/global.css",
+  inlineCSS: true,
+  postcss: {
+    plugins: [
+      require('tailwindcss'),
+    ]
+  }
+})
