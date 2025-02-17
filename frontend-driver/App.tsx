dc5b5dab8b9d01d1bcc967c5cee7b93b4d16cc18
luan0811 <nguyentranminhluan9a10@gmail.com>
2025-02-17 13:05:17 +0700

    setup native

--- frontend-driver/App.tsx
+++ frontend-driver/App.tsx
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Driver App</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-600">
+        Driver App with NativeWind
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
