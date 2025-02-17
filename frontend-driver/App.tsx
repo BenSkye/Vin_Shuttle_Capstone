c5d131f862dbe9e0f3aa25cc754075088b4e39e3
luan0811 <nguyentranminhluan9a10@gmail.com>
2025-02-17 16:14:15 +0700

    setup driver

--- frontend-driver/App.tsx
+++ frontend-driver/App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { View, Text } from 'react-native';
-
+import "./global.css"
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
