c0b3dbfe42244c7d6cd75299e7ee9b643abb2b2a
gkhanh07 <gkhanh0706@gmail.com>
2025-04-07 23:48:15 +0700

    fix bug

--- frontend-manager/src/app/(dashboard)/customers/page.tsx
+++ frontend-manager/src/app/(dashboard)/customers/page.tsx
@@ -9,7 +9,7 @@ import Link from "next/link";
 import { Column } from '@/interfaces/index';
 import { Customer } from "@/interfaces/index";
 import { getCustomer } from "@/services/api/user";
-
+//yessir
 const columns: Column<Customer>[] = [
     { header: "ID", accessor: "id", className: "hidden md:table-cell" },
     { header: "Name", accessor: "name" },
