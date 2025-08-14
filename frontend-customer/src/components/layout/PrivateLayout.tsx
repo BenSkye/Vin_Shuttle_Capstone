41f82db5c369e106231f3c4202591e105c613b8a
gkhanh07 <gkhanh0706@gmail.com>
2025-08-14 15:36:26 +0700

    fix

--- frontend-customer/src/components/layout/PrivateLayout.tsx
+++ frontend-customer/src/components/layout/PrivateLayout.tsx
@@ -1,41 +1,9 @@
 'use client'
 
-import { useEffect } from 'react'
-
-import { Spin } from 'antd'
-import { useRouter } from 'next/navigation'
-
-import { Routes } from '@/constants/routers'
-
-import { useAuth } from '@/context/AuthContext'
-
 interface PrivateLayoutProps {
   children: React.ReactNode
 }
 
 export default function PrivateLayout({ children }: PrivateLayoutProps) {
-  const { isLoggedIn, isLoading } = useAuth()
-  const router = useRouter()
-
-  useEffect(() => {
-    console.log('PrivateLayout: isLoggedIn', isLoggedIn)
-    console.log('PrivateLayout: isLoading', isLoading)
-    if (!isLoading && !isLoggedIn) {
-      router.replace(Routes.AUTH.LOGIN)
-    }
-  }, [isLoggedIn, isLoading, router])
-
-  if (isLoading) {
-    return (
-      <div className="flex min-h-screen items-center justify-center">
-        <Spin size="large" tip="Đang kiểm tra đăng nhập..." className="text-gray-600" />
-      </div>
-    )
-  }
-
-  if (!isLoggedIn) {
-    return null
-  }
-
   return <>{children}</>
 }
